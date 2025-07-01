5196bf0ea1a82dfbe18ce82fcdaf055f29ebde31
Nils Hasenbanck <nils.hasenbanck@easybill.de>
2025-07-01 16:20:23 +0200

    Memory allocator for PPMd7 ported

--- src/decoder_7.rs
+++ src/decoder_7.rs
@@ -1,17 +1,15 @@
-use std::{io::Read, mem::ManuallyDrop};
-
 use crate::{
     byte_reader::ByteReader,
-    internal::ppmd7::{alloc, construct, decode_symbol, free, range_decoder_init, Init, PPMd7},
-    memory::Memory,
+    internal::ppmd7::{decode_symbol, range_decoder_init, PPMd7, RangeCoder, RangeDecoder},
     Error, PPMD7_MAX_MEM_SIZE, PPMD7_MAX_ORDER, PPMD7_MIN_MEM_SIZE, PPMD7_MIN_ORDER, SYM_END,
 };
+use std::io::Read;
+use std::mem::ManuallyDrop;
 
 /// A decoder to decompress data using PPMd7 (PPMdH) with the 7z range coder.
 pub struct Ppmd7Decoder<R: Read> {
     ppmd: PPMd7,
     reader: ByteReader<R>,
-    memory: Memory,
     finished: bool,
 }
 
@@ -27,54 +25,36 @@ impl<R: Read> Ppmd7Decoder<R> {
             return Err(Error::InvalidParameter);
         }
 
-        let mut ppmd = unsafe { std::mem::zeroed::<PPMd7>() };
-        unsafe { construct(&mut ppmd) };
-
-        let mut memory = Memory::new(mem_size);
-
-        let success = unsafe { alloc(&mut ppmd, mem_size, memory.allocation()) };
-
-        if success == 0 {
-            return Err(Error::MemoryAllocation);
-        }
-
         let mut reader = ByteReader::new(reader);
-        let range_decoder = unsafe { &mut ppmd.rc.dec };
-        range_decoder.stream = reader.byte_in_ptr();
+        let mut decoder = RangeDecoder {
+            range: 0,
+            code: 0,
+            low: 0,
+            stream: reader.byte_in_ptr(),
+        };
 
-        let success = unsafe { range_decoder_init(&mut ppmd.rc.dec) };
+        let success = unsafe { range_decoder_init(&mut decoder) };
 
         if success == 0 {
             return Err(Error::RangeDecoderInitialization);
         }
 
-        unsafe { Init(&mut ppmd, order) };
+        let ppmd = PPMd7::new(RangeCoder { dec: decoder }, order, mem_size)?;
 
         Ok(Self {
             ppmd,
             reader,
-            memory,
             finished: false,
         })
     }
 
     /// Returns the inner reader.
     pub fn into_inner(self) -> R {
-        let mut manual_drop_self = ManuallyDrop::new(self);
-        unsafe {
-            free(
-                &mut manual_drop_self.ppmd,
-                manual_drop_self.memory.allocation(),
-            )
-        }
+        let manual_drop_self = ManuallyDrop::new(self);
         let reader = unsafe { std::ptr::read(&manual_drop_self.reader) };
+        let ppmd = unsafe { std::ptr::read(&manual_drop_self.ppmd) };
+        drop(ppmd);
         reader.inner.reader
-    }
-}
-
-impl<R: Read> Drop for Ppmd7Decoder<R> {
-    fn drop(&mut self) {
-        unsafe { free(&mut self.ppmd, self.memory.allocation()) }
     }
 }
 
